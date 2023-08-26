87c8354a02d195f70acc92bef9b3276685461b3f
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2023-08-26 02:13:01 -0300

    feat/Creating AppBar

--- lib/src/home/home_Page.dart
+++ lib/src/home/home_Page.dart
@@ -5,6 +5,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xff00B4CC),
+        title: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 8),
+                border: InputBorder.none,
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+              ),
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: null,
+    );
   }
 }
