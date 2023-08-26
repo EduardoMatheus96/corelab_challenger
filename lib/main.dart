63ba4036c0ef009c9bf31dca87a0dcf86be24dd0
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2023-08-26 01:58:25 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:corelab_app_challenge/src/appwidget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
---
+++ lib/src/appwidget.dart
@@ -0,0 +1,17 @@
+import 'package:corelab_app_challenge/src/home/home_Page.dart';
+
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Marketing place for dentists',
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
